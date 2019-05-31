e98f80b5a45ca456ea1f1a7129752cc609c592f6
kca8869 <karmijo8869@gmail.com>
2019-05-31 18:35:47 -0500

    homepage finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+
+var videoLink=$(".header__video-bg");
+var videoModal=$(".video-modal");
+var video=$(".video-modal__iframe");
+var src=video[0].src;
+console.log(src);
+
+$(".header__video-bg").click(function(x){
+  x.preventDefault();
+  $(video).attr("src",src);
+  $(".video-modal").fadeIn();
+})
+
+$(".video-modal").click(function(x){
+  x.preventDefault();
+  $(video).attr("src",'');
+  $(".video-modal").fadeOut();
+})
