b8dbad805d7df77c4653440255855312b1956b21
kca8869 <karmijo8869@gmail.com>
2019-06-06 10:57:17 -0500

    scroll reveal added

--- js/main.concat.js
+++ js/main.concat.js
@@ -1,9 +1,8 @@
-
+//display/hide video modal
 var videoLink=$(".header__video-bg");
 var videoModal=$(".video-modal");
 var video=$(".video-modal__iframe");
 var src=video[0].src;
-console.log(src);
 
 $(".header__video-bg").click(function(x){
   x.preventDefault();
@@ -15,6 +14,28 @@ $(".video-modal").click(function(x){
   x.preventDefault();
   $(video).attr("src",'');
   $(".video-modal").fadeOut();
+})
+  //scroll reveal Function
+//check if element is in the viewport to start the animation
+function isInWindow(elem) {
+  var limits = elem.getBoundingClientRect();
+  if (limits.top < window.innerHeight) {
+    return true;
+  }
+}
+//scroll reveal function takes an element as a parameter
+//and adds the animation if it comes into the viewport
+function sr(elem) {
+  if (isInWindow(elem)) {
+    $(elem).addClass("animate-sr");
+  }
+}
+//runs the scroll reveal function on all elements with
+//.sr on window scroll.
+$(window).scroll(function () {
+  $(".sr").each(function(i,value){
+    sr(value);
+  });
 })
 
 /*! jQuery v3.4.1 | (c) JS Foundation and other contributors | jquery.org/license */
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
+//display/hide video modal
 var videoLink=$(".header__video-bg");
 var videoModal=$(".video-modal");
 var video=$(".video-modal__iframe");
 var src=video[0].src;
-console.log(src);
 
 $(".header__video-bg").click(function(x){
   x.preventDefault();
@@ -15,4 +14,26 @@ $(".video-modal").click(function(x){
   x.preventDefault();
   $(video).attr("src",'');
   $(".video-modal").fadeOut();
+})
+  //scroll reveal Function
+//check if element is in the viewport to start the animation
+function isInWindow(elem) {
+  var limits = elem.getBoundingClientRect();
+  if (limits.top < window.innerHeight) {
+    return true;
+  }
+}
+//scroll reveal function takes an element as a parameter
+//and adds the animation if it comes into the viewport
+function sr(elem) {
+  if (isInWindow(elem)) {
+    $(elem).addClass("animate-sr");
+  }
+}
+//runs the scroll reveal function on all elements with
+//.sr on window scroll.
+$(window).scroll(function () {
+  $(".sr").each(function(i,value){
+    sr(value);
+  });
 })
